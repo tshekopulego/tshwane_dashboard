5108dfc452d148545d98103107a2408882043d75
Nathaniel <coders@design2code.co.za>
2016-08-26 13:30:24 +0200

    no message

--- application/views/capacity_report.php
+++ application/views/capacity_report.php
@@ -245,7 +245,7 @@
                                 'image':image
                             }, function(data){
                                 window.open(
-                                    '/index.php/capacity/export_pdf/'+data+'/'+selectedDate,
+                                    '/capacity/export_pdf/'+data+'/'+selectedDate,
                                     '_blank' 
                                   );
                             }
